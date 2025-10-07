25dd67db2f6af865ee99bd4333511b0b14163246
januszbratny-dot <janusz.bratny@gmail.com>
2025-10-07 10:25:54 +0200

    Update sloty_gantt_5_4.py

--- sloty_gantt_5_4.py
+++ sloty_gantt_5_4.py
@@ -681,7 +681,7 @@ else:
 
 
 # ---------------------- AUTO-FILL FULL DAY (BEZPIECZNY) ----------------------
-st.subheader("⚡ Automatyczne dociążenie wszystkich brygad (przyspieszonie testowania)")
+st.subheader("⚡ Automatyczne dociążenie wszystkich brygad (przyspieszenie testowania)")
 
 # wybór dnia do autofill
 day_autofill = st.date_input(
